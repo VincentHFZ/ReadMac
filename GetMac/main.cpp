2e0ef1a327e3d3676e729ad62de2e3bd97e3008f
Vincent <534050016@qq.com>
2017-06-01 16:59:06 +0800

    add project

---
+++ GetMac/main.cpp
@@ -0,0 +1,102 @@
+#pragma once
+#pragma execution_character_set("utf-8")
+
+#include <WinSock2.h>
+#include <IPHlpApi.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#pragma comment(lib,"IPHLPAPI.lib")
+
+using namespace std;
+
+bool GetMacByGetAdaptersInfo(string &macOUT)
+{
+	bool ret = false;
+
+	ULONG ulOutBufLen = sizeof(IP_ADAPTER_INFO);
+	PIP_ADAPTER_INFO pAdapterInfo = (IP_ADAPTER_INFO *)malloc(sizeof(IP_ADAPTER_INFO));
+	if (pAdapterInfo == NULL)
+	{
+		return false;
+	}
+
+	if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW)
+	{
+		free(pAdapterInfo);
+		pAdapterInfo = (IP_ADAPTER_INFO *)malloc(ulOutBufLen);
+		if (pAdapterInfo == NULL)
+			return false;
+	}
+	//if(GetAdaptersInfo = (IP_ADAPTER_INFO *)malloc())
+
+	if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == NO_ERROR)
+	{
+		for (PIP_ADAPTER_INFO pAdapter = pAdapterInfo; pAdapter != NULL; pAdapter = pAdapter->Next)
+		{
+			if (pAdapter->Type != MIB_IF_TYPE_ETHERNET)
+			{
+				continue;
+			}
+			if (pAdapter->AddressLength != 6)
+			{
+				continue;
+			}
+			char acMac[32];
+			sprintf(acMac, "%02X-%02X-%02X-%02X-%02X-%02X",
+				int(pAdapter->Address[0]),
+				int(pAdapter->Address[1]),
+				int(pAdapter->Address[2]),
+				int(pAdapter->Address[3]),
+				int(pAdapter->Address[4]),
+				int(pAdapter->Address[5])
+				);
+			ret = true;
+			macOUT = acMac;
+			break;
+		}
+	}
+
+	free(pAdapterInfo);
+
+	return ret;
+}
+
+int main(int argc, char * argt)
+{
+	string acmac;
+	bool ret = true;
+
+	//cout << "The Mac Address: " << endl;
+
+	ret = GetMacByGetAdaptersInfo(acmac);
+
+	if (ret == true)
+	{
+		ofstream out("lisence");
+		if (out.is_open())
+		{
+			for (auto &r : acmac)
+			{
+				out << r;
+			}
+			out.close();
+			cout << "generate lisence file succeed!" << endl;
+			cout << "The MAC addres is: ";
+			for (auto &r : acmac)
+			{
+				cout << r;
+			}
+			cout << endl;
+			system("pause");
+		}
+	}
+	else
+	{
+		cout << "failed" << endl;
+		system("pause");
+	}
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ReadMac
 Read PC MAC address and write it to a file
+
+IDE: Visual Studio 2015 Community
+Language: C++
+System Envirement: Windows 2000 and above
+
+
